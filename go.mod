1360e79401c61fcb089219597ce66785a559c23c
Hugo Santos <hugo@namespacelabs.com>
2023-11-08 10:12:57 -0600

    added network/unixproxy.

--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.20
 
 require (
 	cloud.google.com/go/secretmanager v1.11.1
+	golang.org/x/sys v0.11.0
 	google.golang.org/api v0.136.0
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230807174057-1744710a1577
 	google.golang.org/grpc v1.57.0
 	inet.af/tcpproxy v0.0.0-20231102063150-2862066fc2a9
+	namespacelabs.dev/go-ids v0.0.0-20221124082625-9fc72ee06af7
 )
 
 require (
@@ -20,12 +22,13 @@ require (
 	github.com/google/s2a-go v0.1.4 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.5 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
+	github.com/jxskiss/base62 v1.1.0 // indirect
+	github.com/segmentio/ksuid v1.0.4 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/crypto v0.12.0 // indirect
 	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/oauth2 v0.11.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230803162519-f966b187b2e5 // indirect
---
+++ network/unixproxy/unixproxy.go
@@ -0,0 +1,127 @@
+// Copyright 2022 Namespace Labs Inc; All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+
+package cluster
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+
+	"golang.org/x/sys/unix"
+	"namespacelabs.dev/go-ids"
+	"namespacelabs.dev/integrations/network/netcopy"
+)
+
+type Proxy struct {
+	TempDir    string
+	SocketAddr string
+}
+
+type ProxyOpts struct {
+	Debug, Errors  io.Writer
+	SocketPath     string
+	Kind           string
+	Blocking       bool
+	Connect        func(context.Context) (net.Conn, error)
+	AnnounceSocket func(string)
+}
+
+func RunProxy(ctx context.Context, opts ProxyOpts) (*Proxy, error) {
+	socketPath := opts.SocketPath
+
+	if err := unix.Unlink(socketPath); err != nil && !os.IsNotExist(err) {
+		return nil, err
+	}
+
+	var d net.ListenConfig
+	listener, err := d.Listen(ctx, "unix", socketPath)
+	if err != nil {
+		return nil, err
+	}
+
+	if opts.AnnounceSocket != nil {
+		opts.AnnounceSocket(socketPath)
+	}
+
+	if opts.Blocking {
+		ch := make(chan struct{})
+		go func() {
+			select {
+			case <-ch:
+			case <-ctx.Done():
+			}
+			_ = listener.Close()
+		}()
+
+		defer close(ch)
+
+		if err := serveProxy(ctx, opts.Debug, opts.Errors, listener, func(ctx context.Context) (net.Conn, error) {
+			return opts.Connect(ctx)
+		}); err != nil {
+			if ctxErr := ctx.Err(); ctxErr != nil {
+				return nil, ctxErr
+			}
+
+			return nil, err
+		}
+
+		return nil, nil
+	} else {
+		go func() {
+			if err := serveProxy(ctx, opts.Debug, opts.Errors, listener, func(ctx context.Context) (net.Conn, error) {
+				return opts.Connect(ctx)
+			}); err != nil {
+				log.Fatal(err)
+			}
+		}()
+
+		return &Proxy{filepath.Dir(socketPath), socketPath}, nil
+	}
+}
+
+func serveProxy(ctx context.Context, debugLog, errorLog io.Writer, listener net.Listener, connect func(context.Context) (net.Conn, error)) error {
+	for {
+		rawConn, err := listener.Accept()
+		if err != nil {
+			return err
+		}
+
+		conn := withAddress{rawConn, "local connection"}
+
+		go func() {
+			var d netcopy.DebugLogFunc
+
+			id := ids.NewRandomBase32ID(4)
+			fmt.Fprintf(debugLog, "[%s] new connection\n", id)
+
+			d = func(format string, args ...any) {
+				fmt.Fprintf(debugLog, "["+id+"]: "+format+"\n", args...)
+			}
+
+			defer conn.Close()
+
+			peerConn, err := connect(ctx)
+			if err != nil {
+				fmt.Fprintf(errorLog, "Failed to connect: %v\n", err)
+				return
+			}
+
+			defer peerConn.Close()
+
+			_ = netcopy.CopyConns(d, conn, peerConn)
+		}()
+	}
+}
+
+type withAddress struct {
+	net.Conn
+	addrDesc string
+}
+
+func (w withAddress) RemoteAddrDebug() string { return w.addrDesc }
