32e40a11ea2f7186f40f89e0039faf4a753ee138
Hugo Santos <hugo@namespacelabs.com>
2025-07-22 22:49:05 +0200

    auth: make ExtractClaims public. (#22)

--- auth/claims.go
+++ auth/claims.go
@@ -2,8 +2,6 @@ package auth
 
 import (
 	"errors"
-	"fmt"
-	"io"
 	"strings"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -20,25 +18,24 @@ type TokenClaims struct {
 	PrimaryRegion string `json:"primary_region"`
 }
 
-func tokenClaims(debugLog io.Writer, token string) (*TokenClaims, error) {
+func ExtractClaims(token string) (*TokenClaims, error) {
 	switch {
 	case strings.HasPrefix(token, "st_"):
-		return parseClaims(debugLog, strings.TrimPrefix(token, "st_"))
+		return parseClaims(strings.TrimPrefix(token, "st_"))
 	case strings.HasPrefix(token, "nsct_"):
-		return parseClaims(debugLog, strings.TrimPrefix(token, "nsct_"))
+		return parseClaims(strings.TrimPrefix(token, "nsct_"))
 	case strings.HasPrefix(token, "nscw_"):
-		return parseClaims(debugLog, strings.TrimPrefix(token, "nscw_"))
+		return parseClaims(strings.TrimPrefix(token, "nscw_"))
 	default:
 		return nil, ErrNotLoggedIn
 	}
 }
 
-func parseClaims(debugLog io.Writer, raw string) (*TokenClaims, error) {
+func parseClaims(raw string) (*TokenClaims, error) {
 	parser := jwt.Parser{}
 
 	var claims TokenClaims
 	if _, _, err := parser.ParseUnverified(raw, &claims); err != nil {
-		fmt.Fprintf(debugLog, "parsing claims %q failed: %v\n", raw, err)
 		return nil, ErrNotLoggedIn
 	}
 
